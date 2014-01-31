970b68d7d05f4970ac00e5f280f5bb77e42859f6
Justin <jyang772@gmail.com>
2014-01-31 02:06:23 -0800

    Initial Commit

---
+++ Assignment_0_Program_1/src/UserNameGen.java
@@ -0,0 +1,64 @@
+//Justin Yang
+//Assignment 0 
+//Program #1
+// 1/14/14
+
+//Hello
+
+//Troll
+
+
+import java.util.Scanner;
+import java.util.Random; 
+
+
+public class UserNameGen {
+
+	public static void main(String[] args) {
+		
+		String firstName = "", lastName = "", userName="";
+		boolean done = false;
+		
+		System.out.println("Enter your first name: ");
+		
+		Scanner fooBar = new Scanner(System.in);
+		firstName = fooBar.next();
+		
+		System.out.println("Enter your last name: ");
+		lastName = fooBar.next();
+		
+		do{
+		
+			if(lastName.length() < 5)
+		{	
+				System.out.println("Enter your last name: ");
+				lastName = fooBar.next();
+		}
+			else
+				done = true;
+		
+		}while(done == false);
+			
+			
+		Random ran = new Random();
+		int x = ran.nextInt(99) + 10;
+		
+		userName = firstName.substring(0,1) + lastName.substring(0,4) + x;
+		
+		
+		
+		
+		System.out.println("Your username: " );
+		System.out.println(userName);
+		
+		
+		
+		
+		
+		
+		System.out.println("Done");
+		
+
+	}
+
+}
